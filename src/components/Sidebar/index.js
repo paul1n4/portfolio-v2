d9698122ea944fa668b9f7cfb4411de68d2bf000
paul1n4 <paulinevaleroc@gmail.com>
2025-01-15 15:39:22 -0500

    first commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+//import { Link } from 'react-router-dom'
+import './index.scss'
+
+const Sidebar = () => {
+  return (
+    <div className='nav-bar'>
+      
+    </div>
+  )
+}
+
+export default Sidebar
