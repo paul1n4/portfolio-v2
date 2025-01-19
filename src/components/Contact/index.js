86c15c614c95c532fe521a0e5b1536f78e9f6a16
paul1n4 <paulinevaleroc@gmail.com>
2025-01-19 18:53:20 -0500

    change index.html title

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
+import { faBehance, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 const Contact = () => {
   return (
@@ -6,8 +8,25 @@ const Contact = () => {
       <h2>Contact Me</h2>
       <div className='contact-container'>
         <div className='contact-text'>
-          <p>I'd love to hear from you! Whether you have a project in mind, a question about my work, or just want to connect, feel free to reach out. </p>
-
+          <p>I'd love to hear from you! Whether you have a project in mind, a question about my work, or just want to connect. </p>
+          <div className='contact-media'>
+           
+              <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/paulinevalero/'>
+                <FontAwesomeIcon icon={faLinkedinIn} />
+                <span>/linkedin</span>
+                </a>
+           
+              <a target='_blank' rel='noreferrer' href='https://github.com/paul1n4'>
+                <FontAwesomeIcon icon={faGithub} />
+                <span>/github</span>
+              </a>
+          
+              <a target='_blank' rel='noreferrer' href='https://www.behance.net/paulinevalero'>
+                <FontAwesomeIcon icon={faBehance} />
+                <span>/behance</span>
+              </a>
+            
+          </div>
         </div>
       </div>
     </div>
