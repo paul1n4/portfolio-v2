178e15cb757a87adfcc74a57e0c71a48de6183c3
paul1n4 <paulinevaleroc@gmail.com>
2025-01-19 18:03:42 -0500

    Start Contac page & fix media queries in home page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+
+const Contact = () => {
+  return (
+    <div className='container contact-page'>
+      <h2>Contact Me</h2>
+      <div className='contact-container'>
+        <div className='contact-text'>
+          <p>I'd love to hear from you! Whether you have a project in mind, a question about my work, or just want to connect, feel free to reach out. </p>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
