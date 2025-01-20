637408899793ae05cb8b02f401e9ee15c5959c31
paul1n4 <paulinevaleroc@gmail.com>
2025-01-20 16:14:01 -0500

    Add form in contact & media queries in contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -10,12 +10,12 @@ const Contact = () => {
         <div className='contact-text'>
           <p>I'd love to hear from you! Whether you have a project in mind, a question about my work, or just want to connect. </p>
           <div className='contact-media'>
-           
+            
               <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/paulinevalero/'>
                 <FontAwesomeIcon icon={faLinkedinIn} />
                 <span>/linkedin</span>
                 </a>
-           
+          
               <a target='_blank' rel='noreferrer' href='https://github.com/paul1n4'>
                 <FontAwesomeIcon icon={faGithub} />
                 <span>/github</span>
@@ -27,6 +27,21 @@ const Contact = () => {
               </a>
             
           </div>
+        </div>
+        
+        <div className='form-container'>
+          <form>
+            <div className='half-input'>
+              <input type='text' name='name' placeholder='Name' required/>
+              <input type='email' name='email' placeholder='Email' required/>
+            </div>
+           
+            <input type='text' name='subject' placeholder='Subject' required/>
+            <textarea name='message' placeholder='Message' required></textarea>
+            <div className='contact-submit'>
+              <input type='submit'className='flat-button' value='SEND'/>
+            </div>
+          </form>
         </div>
       </div>
     </div>
